85dd4b765f9cb9156c9603e8b326d56ccfc46cf5
Andres <afarangurens@unal.edu.co>
2022-03-26 19:22:06 -0500

    Caecar's cipher

--- Talleres/CaesarsCipher.py
+++ Talleres/CaesarsCipher.py
@@ -6,6 +6,39 @@ menu_options = {
         3: 'Exit'
 }
 
+# Code to separate a string in separated english words
+# Code accepted answer at: https://stackoverflow.com/questions/8870261/how-to-split-text-without-spaces-into-list-of-words
+def infer_spaces(s):
+    words = open("words-by-frequency.txt").read().split()
+    wordcost = dict((k, log((i + 1) * log(len(words)))) for i, k in enumerate(words))
+    maxword = max(len(x) for x in words)
+    """Uses dynamic programming to infer the location of spaces in a string
+    without spaces."""
+
+    # Find the best match for the i first characters, assuming cost has
+    # been built for the i-1 first characters.
+    # Returns a pair (match_cost, match_length).
+    def best_match(i):
+        candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
+        return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
+
+    # Build the cost array.
+    cost = [0]
+    for i in range(1,len(s)+1):
+        c,k = best_match(i)
+        cost.append(c)
+
+    # Backtrack to recover the minimal-cost string.
+    out = []
+    i = len(s)
+    while i>0:
+        c,k = best_match(i)
+        assert c == cost[i]
+        out.append(s[i-k:i])
+        i -= k
+
+    return " ".join(reversed(out))
+
 
 def print_menu():
     print("Welcome to Caesar's Cipher, choose your option: ")
@@ -49,7 +82,7 @@ def caesars_cipher():
             print(" Enter the cipher's key")
             k = int(input())
             print("The decrypted message is: ")
-            print_message(decrypt(message, k))
+            print(infer_spaces("".join(decrypt(message, k))))
             print("\n")
 
         elif option == 3:
@@ -59,10 +92,5 @@ def caesars_cipher():
             print('Please enter a number between 1 and 3.')
 
 
-def bullshit_code(message):
-    words = open("words-by-frequency.txt").read().split()
-
-#caesars_cipher()
-
-a = "itisclaimedtheearliestknownreferencetothistypeofcipherisinthekamasutrawhichsayswomenshouldlearntheartofsecretwritingtoconcealtheirliasons"
+caesars_cipher()
 
--- Talleres/PlayFair.py
+++ Talleres/PlayFair.py
@@ -1,11 +1,46 @@
 import numpy as np
 import functools as ft
+from math import log
 
 menu_options = {
         1: 'Encrypt a message',
         2: 'Decrypt a message',
         3: 'Exit',
     }
+
+
+# Code to separate a string in separated english words
+# Code accepted answer at: https://stackoverflow.com/questions/8870261/how-to-split-text-without-spaces-into-list-of-words
+def infer_spaces(s):
+    words = open("words-by-frequency.txt").read().split()
+    wordcost = dict((k, log((i + 1) * log(len(words)))) for i, k in enumerate(words))
+    maxword = max(len(x) for x in words)
+    """Uses dynamic programming to infer the location of spaces in a string
+    without spaces."""
+
+    # Find the best match for the i first characters, assuming cost has
+    # been built for the i-1 first characters.
+    # Returns a pair (match_cost, match_length).
+    def best_match(i):
+        candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
+        return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
+
+    # Build the cost array.
+    cost = [0]
+    for i in range(1,len(s)+1):
+        c,k = best_match(i)
+        cost.append(c)
+
+    # Backtrack to recover the minimal-cost string.
+    out = []
+    i = len(s)
+    while i>0:
+        c,k = best_match(i)
+        assert c == cost[i]
+        out.append(s[i-k:i])
+        i -= k
+
+    return " ".join(reversed(out))
 
 
 # Removes the duplicates in a list of characters whilst maintaining insertion order
@@ -189,7 +224,8 @@ def play_fair():
             key = list(input().lower().replace(" ", "").replace("j", "i"))
             decrypted = decrypt(message, key)
             print("The decrypted message is: ")
-            print("".join(decrypted))
+            mess = "".join(decrypted)
+            print(infer_spaces(mess))
             print("\n")
 
         elif option == 3:
