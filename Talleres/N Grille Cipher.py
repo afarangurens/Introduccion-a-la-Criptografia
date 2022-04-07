9b7bf5ddf16ba2c304e40b7d51e7855150a8eeca
Andres <afarangurens@unal.edu.co>
2022-04-07 05:32:57 -0500

    N Grille Cipher started

---
+++ Talleres/N Grille Cipher.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import numpy as np
+
+grille = []
+
+
+def initialize_grille(n):
+    global grille
+    grille = [[0 for i in range(n)] for j in range(n)]
+
+
+def click(row, col):
+    global grille
+
+    grille[row][col] = 1
+    print(np.matrix(grille))
+
+
+n = 5
+
+initialize_grille(n)
+root = tk.Tk()
+for row in range(n):
+    for col in range(n):
+        button = tk.Button(root, text="%s,%s" % (row, col),
+                           command=lambda row=row, col=col: click(row, col))
+        button.grid(row=row, column=col, sticky="nsew")
+
+init_grill_button = tk.Button(root, text="Reset Grille", command=lambda: initialize_grille(n))
+init_grill_button.grid(row=0, column=n+1)
+label = tk.Label(root, text="")
+label.grid(row=n, column=0, columnspan=n, sticky="new")
+
+root.grid_rowconfigure(n, weight=1)
+root.grid_columnconfigure(n, weight=1)
+
+
+
+root.mainloop()
